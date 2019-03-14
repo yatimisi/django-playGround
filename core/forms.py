12e81e1304ed82feeab5eb1ea17b51d03947acc3
Yatimisi <yatimisi2012@gmail.com>
2019-03-14 17:46:19 +0800

    refactor: use cleaned_data for BmiForm

--- core/forms.py
+++ core/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class BmiForm(forms.Form):
-    heigh = forms.FloatField(min_value=0, label='身高')
-    kg = forms.FloatField(min_value=0, label='體重')
+    heigh = forms.FloatField(min_value=1, label='身高')
+    kg = forms.FloatField(min_value=1, label='體重')
--- core/views.py
+++ core/views.py
@@ -9,19 +9,12 @@ def hello(request):
 
 # http://127.0.0.1:8000/bmi/?kg=65&heigh=179
 def bmi(request):
-    if request.method == 'POST':
-        heigh = request.POST.get('heigh', '')
-        kg = request.POST.get('kg', '')
-        if heigh == '' or kg == '':
-            return HttpResponse("heigh or kg is None")
-        heigh = float(heigh)
-        kg = float(kg)
-        bmi = kg / ((heigh / 100) ** 2)
-        return render(request, 'bmi.html', {
-            'bmi': bmi, 
-        })
+    form = BmiForm(request.POST or None)
+    if form.is_valid():
+        heigh = form.cleaned_data['heigh']
+        kg = form.cleaned_data['kg']
+        bmi = w / ((h / 100) ** 2)
+        return render(request, 'bmi.html', {'bmi': bmi})
 
-    form = BmiForm()
-    return render(request, 'bmi-input.html',{
-        'form': form
-    })
+    return render(request, 'bmi-input.html', {'form': form})
+
